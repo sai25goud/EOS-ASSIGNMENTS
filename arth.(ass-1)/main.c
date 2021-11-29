d8c5837fdd382df54720630cbba791631914369a
sai25goud <saikiran25goud@gmail.com>
2021-11-29 19:29:51 +0530

    day1

---
+++ arth.(ass-1)/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+  
+    int main(int argc, char const *argv[])
+{
+    int result =0;
+
+    result = add(10, 20);
+    printf("addition: %d\n",result);
+
+     result =sub(10,20);
+    printf("subtraction: %d\n",result);
+    return 0;
+}
+
