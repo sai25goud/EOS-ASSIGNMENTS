b675a04d51cf1df52b987ab918f66479b1a93c82
sai25goud <saikiran25goud@gmail.com>
2021-12-02 14:31:31 +0530

    day7 assignment

--- pthread ass./thread.c
+++ pthread ass./thread.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<pthread.h>
 
-void *hello_thread(void *arg)
+/*void *hello_thread(void *arg)
 {
    printf("Hello thread\n");
 }
 void *bye_thread(void *arg)
 {
     printf("Bye thread\n");
-}
+}*/
+  void *greet_thread(void *arg)
+  {
+  printf("%s thread\n",arg);
+  }
   int main(int argc,char const *argv[])
   {
       pthread_t helloID;
@@ -16,9 +20,9 @@ void *bye_thread(void *arg)
       printf("main thread:Before creating hello thread\n");
       printf("main thread:Before creating bye thread\n");
 
-      pthread_create(&helloID,NULL,hello_thread,NULL);
+      pthread_create(&helloID,NULL,greet_thread,"Hello");
       pthread_join(helloID,NULL);
-      pthread_create(&byeID,NULL,bye_thread,NULL);
+      pthread_create(&byeID,NULL,greet_thread,"Bye");
       pthread_join(byeID,NULL);
 
       printf("main thread:After creating hello thread\n");
