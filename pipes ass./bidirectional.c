2bd0c3a92936f30230866431cdbc3f87ca306920
sai25goud <saikiran25goud@gmail.com>
2021-12-03 12:42:00 +0530

    day6 assignment

--- pipes ass./bidirectional.c
+++ pipes ass./bidirectional.c
@@ -1,57 +1,57 @@
-#include<unistd.h>
 #include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
-
-int main(int argc,char* argv[])
+#define size 100
+int main(int argc,char const  *argv[])
 {
-    int fd1[2];
-    int fd2[2];
-    pipe(fd1);
-    pipe(fd2);
+    int arr[2],sum;
+    pid_t id;
+    char buff[size];
+    int pid1[2];
+    int pid2[2];
+    pipe(pid1);
+    pipe(pid2);
 
-    //fd[0] --> reading end
-    //fd[1] --> writing end
-    pid_t id=fork();
+    id=fork();
 
     if(id == 0)
     {
         //child process
         
-        int nread;
-        int a,b;
-        nread=read(fd1[0],(int*)&a,sizeof(int));
-        nread=read(fd1[0],(int*)&b,sizeof(int));
-        int res;
-        res=a+b;
-        int nwrite;
-        nwrite=write(fd2[1],(int*)&res,sizeof(int));
-        printf("child process done\n");
+        close(pid1[1]);
+        
+        //read(pid[0],buff,size);
+        read(pid1[0],arr,size);
+        printf("%d %d\n",arr[0],arr[1]);
+        printf("child process\n");
+        close(pid1[0]);
+        
+        close(pid2[0]);
+        sum=arr[0]+arr[1];
+        write(pid2[1],&sum,sizeof(sum));
+        close(pid2[1]);
 
     }
     else 
     {
-        printf("enter two numbers\n");
-        int a,b;
-        scanf("%d",&a);
-        scanf("%d",&b);
-        int nwrite;
-        nwrite=write(fd1[1],(int*)&a,sizeof(int));
-        nwrite=write(fd1[1],(int*)&b,sizeof(int));
+        printf("parent process\n");
+        close(pid1[0]);
         
-        if(nwrite==-1)
-        {
-            printf("failed writing to pipe\n");
-            return -1;
-        }
+        printf("enter two integers:");
+        scanf("%d %d",&arr[0],&arr[1]);
+        write(pid1[1],arr,sizeof(arr));
         
-        int nread;
-        int res;
-        nread=read(fd2[0],(int*)&res,sizeof(int));
-        printf("res is %d\n",res);
-        printf("parent process done\n");
+        printf("datasent successfully\n");
+        close(pid1[1]);
+        
+        close(pid2[1]);
+        read(pid2[0],&sum,sizeof(sum));
+        printf("addition=%d\n",sum);
+        printf("data received from child successfully \n");
     }
-    close(fd1);
-    close(fd2);
     
-    return 1;
+    return 0;
 }
