cff47f5189fd7e93b4228460284418bec6e9de92
sai25goud <saikiran25goud@gmail.com>
2021-12-08 19:18:23 +0530

    day11

---
+++ sharedmem/p2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int *count;
+int num;
+int main()
+{
+    int shmd;
+    shmd=shm_open("/cdacshm",O_RDWR | O_CREAT,S_IRUSR | S_IWUSR);
+    ftruncate(shmd,sizeof(int));
+    count=(int *)mmap(NULL,sizeof(int),PROT_READ|PROT_WRITE,MAP_SHARED,shmd,0);
+    while(1)
+    {
+        printf("count:%d\n",*count);
+        sleep(1);
+    }
+    return 0;
+}
